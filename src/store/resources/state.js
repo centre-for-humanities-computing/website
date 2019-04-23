b8fdcfa0a428363d6446e576890bc99600e2d5c7
Max Eckardt <max@cas.au.dk>
2019-04-23 14:41:33 +0200

    playlist thumbnails

--- src/store/resources/state.js
+++ src/store/resources/state.js
@@ -38,23 +38,23 @@ const state = {
       tag: 'stopWords'
     },
     {
-      title: 'Video Introduction to Data Analysis with Jupyter Notebooks',
+      title: 'Data Analysis with Jupyter Notebooks',
       body: `
-        <a href="https://www.youtube.com/watch?v=Z-T76FQavig&list=PLzmqDzBWP43RxUN39mFip5nWsxlxEoV0J" target="_blank">These videos</a> use the <a href="https://github.com/digitaltxtlab/CLEAR" target="_blank">Computational Literary Repository (CLEAR)</a> to show learners first steps into data analysis with texts.
+        <a href="https://www.youtube.com/watch?v=Z-T76FQavig&list=PLzmqDzBWP43RxUN39mFip5nWsxlxEoV0J" target="_blank">This video course</a> by the Datakuben project uses the <a href="https://github.com/digitaltxtlab/CLEAR" target="_blank">Computational Literary Repository (CLEAR)</a> to show learners first steps into data analysis with texts.
         The course covers installation of <a href="https://jupyter.org/" target="_blank">Jupyter notebooks</a> as part of <a href="https://www.anaconda.com/distribution/" target="_blank">Anaconda</a>, 
         a gentle introduction to its features, and an introduction to data analysis basics as featured by the notebooks in the repository.
       `,
-      src: 'jupyter.png',
+      src: 'jupyter_playlist.png',
       tag: 'jupyter'
     },
     {
-      title: 'Video Course on Text Analysis with Voyant Tools',
+      title: 'Text Analysis with Voyant Tools',
       body: `
-        This Video Course on <a href="https://voyant-tools.org/docs/#!/guide/start" target="_blank">Voyant Tools</a> starts out by describing how to install a local Voyant server for 
+        This Datakuben video course on <a href="https://voyant-tools.org/docs/#!/guide/start" target="_blank">Voyant Tools</a> starts out by describing how to install a local Voyant server for 
         <a href="https://www.youtube.com/watch?v=4urg_apOoJY&list=PLzmqDzBWP43Rsd2jctidmjfkNAsR4NTAu" target="_blank">mac OS X</a> and <a href="https://www.youtube.com/watch?v=8e7M8NqGyF4&list=PLzmqDzBWP43QrknrraIcf9Dd7HY9or3dz" target="_blank">windows</a> users respectively. 
-        The course then continues with by covering <a href="https://www.youtube.com/watch?v=M7UgSJmWHKM&list=PLzmqDzBWP43RDUE6St-51PnXBG2J7shbN" target="_blank">a selection of text operations with Voyant tools</a>.
+        The course then continues with by covering <a href="https://www.youtube.com/watch?v=M7UgSJmWHKM&list=PLzmqDzBWP43RDUE6St-51PnXBG2J7shbN" target="_blank">a selection of text operations with Voyant tools</a>. 
       `,
-      src: 'voyant.png',
+      src: 'voyant_playlist.png',
       tag: 'voyant'
     }
   ]
