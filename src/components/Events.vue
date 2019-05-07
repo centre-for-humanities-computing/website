c29792330a3c7161eeabca33dba22d045b639d9c
Max Eckardt <max@cas.au.dk>
2019-05-07 11:58:23 +0200

    responsive calendar width

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -22,7 +22,7 @@
             <v-card-text>
               <v-layout>
                 <v-flex>
-                  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBerlin&amp;src=N3NlNjU5bG00NTc4MjFpY2gwa203MHRkZDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%237CB342&amp;showNav=1&amp;showDate=1&amp;showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBerlin&amp;src=N3NlNjU5bG00NTc4MjFpY2gwa203MHRkZDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%237CB342&amp;showNav=1&amp;showDate=1&amp;showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
                 </v-flex>
               </v-layout>
             </v-card-text>
