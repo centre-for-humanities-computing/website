72d3d1aeda988fa5d83260958f71404269af7c8b
Max Eckardt <max@cas.au.dk>
2019-06-11 09:27:24 +0200

    new service order

--- src/store/services/state.js
+++ src/store/services/state.js
@@ -33,20 +33,6 @@ const state = {
   ],
   cards: [
     {
-      title: 'Cognitive Services',
-      body: `
-      Do you need to automate analysis of language, audio, images or video? Are your data
-      too complex to deal with by hand? Or do you simply want to experiment with the possibilities
-      of Artificial Intelligence in your research domain. We offer state-of-the arts
-      machine learning and AI to solve your specific problem. Our team research and develop machine learning
-      algorithms with a particular interest in applications in the the humanities. We have worked with neural
-      networks from back when they were only shallow and have since acquired extensive experience with the major
-      deep learning frameworks.
-      `,
-      src: 'HD_chris-liverani-552652-unsplash.jpg',
-      tag: 'cognitiveSupport'
-    },
-    {
       title: 'Project Design and Application Consulting',
       body: `
       As a researcher your knowledge and skills are are best inversted in your research
@@ -60,20 +46,20 @@ const state = {
       `,
       src: 'HD_rawpixel-983726-unsplash.jpg',
       tag: 'consulting'
-    },
-    {
-      title: 'App & Data Hosting',
+    }, {
+      title: 'Cognitive Services',
       body: `
-      We offer infrastructure to host data securely. Currently we work on a custom-built domain-specific compiler for
-      fulltext-searchable text data services. As per our development paradigms this service will be highly scalable, and
-      feature seamless configuration and high usability. We achieve that by using user-maintained database schemas to
-      dynamically assemble full-stack web applications with dedicated data backends, synchronized Lucene text-search
-      indexes, and fully-featured user interfaces for access controlled CRUD operations.
+      Do you need to automate analysis of language, audio, images or video? Are your data
+      too complex to deal with by hand? Or do you simply want to experiment with the possibilities
+      of Artificial Intelligence in your research domain. We offer state-of-the arts
+      machine learning and AI to solve your specific problem. Our team research and develop machine learning
+      algorithms with a particular interest in applications in the the humanities. We have worked with neural
+      networks from back when they were only shallow and have since acquired extensive experience with the major
+      deep learning frameworks.
       `,
-      src: 'HD_imgix-391808-unsplash.png',
-      tag: 'hosting'
-    },
-    {
+      src: 'HD_chris-liverani-552652-unsplash.jpg',
+      tag: 'cognitiveSupport'
+    }, {
       title: 'Web Services',
       body: `
       The CHCAA keeps track of the state-of-the-art in web application technologies and trends. Our domain experts
@@ -84,8 +70,7 @@ const state = {
       `,
       src: 'HD_stephen-dawson-670638-unsplash.jpg',
       tag: 'webService'
-    },
-    {
+    }, {
       title: 'Software Development',
       body: `
       Our research software engineers research and develop algorithms for the innovative implementation of the future's
@@ -96,7 +81,18 @@ const state = {
       `,
       src: 'HD_arif-riyanto-974674-unsplash.jpg',
       tag: 'development'
-    },
+    }, {
+      title: 'App & Data Hosting',
+      body: `
+      We offer infrastructure to host data securely. Currently we work on a custom-built domain-specific compiler for
+      fulltext-searchable text data services. As per our development paradigms this service will be highly scalable, and
+      feature seamless configuration and high usability. We achieve that by using user-maintained database schemas to
+      dynamically assemble full-stack web applications with dedicated data backends, synchronized Lucene text-search
+      indexes, and fully-featured user interfaces for access controlled CRUD operations.
+      `,
+      src: 'HD_imgix-391808-unsplash.png',
+      tag: 'hosting'
+    }
   ]
 }
 export default state
