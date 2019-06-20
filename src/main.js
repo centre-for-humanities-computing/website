3b54e8d3cd514ce0ca3d1c3e88db8ad1b7e2a726
Max Eckardt <max@cas.au.dk>
2019-06-20 10:52:07 +0200

    import loadscript and await loading

--- src/main.js
+++ src/main.js
@@ -7,20 +7,27 @@ import moment from 'moment'
 import _ from 'lodash'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'vuetify/dist/vuetify.min.css'
+import LoadScript from 'vue-plugin-load-script'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 Vue.use(VueAwesomeSwiper)
+Vue.use(LoadScript)
+
 Vue.loadScript('IEShim.js')
+  .then(() => {
+    Vue.prototype.moment = moment
+    Vue.prototype._ = _
 
-Vue.prototype.moment = moment
-Vue.prototype._ = _
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>'
-})
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      router,
+      store,
+      components: { App },
+      template: '<App/>'
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
