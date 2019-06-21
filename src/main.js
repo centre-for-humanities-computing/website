96e0b8d7730d77935acef88c45deb9bf992988c2
Max Eckardt <max@cas.au.dk>
2019-06-21 11:03:56 +0200

    removed loadScript IEShim

--- src/main.js
+++ src/main.js
@@ -7,29 +7,20 @@ import moment from 'moment'
 import _ from 'lodash'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'vuetify/dist/vuetify.min.css'
-import LoadScript from 'vue-plugin-load-script'
 
 Vue.config.productionTip = false
 
-Vue.use(LoadScript)
+Vue.prototype.moment = moment
+Vue.prototype._ = _
 
-Vue.loadScript('IEShim.js')
-  .then(() => {
-    Vue.prototype.moment = moment
-    Vue.prototype._ = _
+Vue.use(Vuetify)
+Vue.use(VueAwesomeSwiper)
 
-    Vue.use(Vuetify)
-    Vue.use(VueAwesomeSwiper)
-
-    /* eslint-disable no-new */
-    new Vue({
-      el: '#app',
-      router,
-      store,
-      components: { App },
-      template: '<App/>'
-    })
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
